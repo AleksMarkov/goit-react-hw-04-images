5e5a1da8480b78510ed88bfe4d4a02f23bef80d8
AleksMarkov <137721438+AleksMarkov@users.noreply.github.com>
2024-01-21 22:16:41 -0500

    updated
    refactored

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,31 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import styles from './modal.module.css';
 
 const modalRoot = document.getElementById('modal-root');
 
+const Modal = ({ close, children }) => {
+  const closeModal = ({ target, currentTarget, code }) => {
+    if (target === currentTarget || code === 'Escape') {
+      close();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', closeModal);
+
+    return () => document.removeEventListener('keydown', closeModal);
+  }, []);
+
+  return createPortal(
+    <div onClick={closeModal} className={styles.overlay}>
+      <div className={styles.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
+/*
 class Modal extends Component {
   componentDidMount() {
     document.addEventListener('keydown', this.closeModal);
@@ -27,9 +48,7 @@ class Modal extends Component {
     return createPortal(
       <div onClick={closeModal} className={styles.overlay}>
         <div className={styles.modal}>
-          {/* <span onClick={close} className={styles.close}>
-            X
-          </span> */}
+          
           {children}
         </div>
       </div>,
@@ -37,5 +56,5 @@ class Modal extends Component {
     );
   }
 }
-
+*/
 export default Modal;
